7e6a901fa861cf3447864e2872535f06dbfd8508
ddl-matthew-tendler <153218207+ddl-matthew-tendler@users.noreply.github.com>
2024-08-21 12:36:30 -0400

    Update views.py
    Line graph version

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 import plotly
 import plotly.express as px
@@ -5,10 +6,9 @@ from flask import render_template
 
 from app import app
 
-
 @app.route("/")
 def homepage():
     df = pd.read_excel("/mnt/data/Dexcom_Readings/DexcomG8Domino.xlsx")
-    fig = px.bar(df, x='Timestamp', y='Glucose Level (mg/dL)', color='Alert', barmode='group')
+    fig = px.line(df, x='Timestamp', y='Glucose Level (mg/dL)', color='Alert')
     graphJSON = fig.to_json()
     return render_template("dashboard.html", graphJSON=graphJSON)
